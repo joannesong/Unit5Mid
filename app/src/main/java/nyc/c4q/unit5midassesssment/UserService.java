ed820b2b1e04e7730053921b3466e14bc7363f6e
Joanne Song <joannesong@ac.c4q.nyc>
2018-01-24 21:59:59 -0500

    change to networking

--- app/src/main/java/nyc/c4q/unit5midassesssment/UserService.java
+++ app/src/main/java/nyc/c4q/unit5midassesssment/UserService.java
@@ -11,14 +11,16 @@ import retrofit2.http.Query;
 
 public interface UserService {
 
-    String us = "us";
-    String info = "name,location,cell,email,dob,picture";
-    int results = 20;
+//    String us = "us";
+//    String info = "name,location,cell,email,dob,picture";
+//    int results = 20;
+//
+//    @GET("api/")
+//    Call<User> getInfo(@Query("nat") String us,
+//                       @Query("inc") String info,
+//                       @Query("results") int results);
 
-    @GET("api/")
-    Call<User> getInfo(@Query("nat") String us,
-                       @Query("inc") String info,
-                       @Query("results") int results);
-
+    @GET("?nat=us&inc=name,location,cell,email,dob,picture&results=20")
+    Call<User> getInfo();
 
 }
